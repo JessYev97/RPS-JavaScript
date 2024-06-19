a426e9d7cef581f939de661106ddb72a6f220868
JessYev97 <jessmyeverino@gmail.com>
2024-06-19 15:44:20 -0500

    Add more pseudocode
    further outline the objectives in pseudocode to be able to proceed

--- game-logic.js
+++ game-logic.js
@@ -13,10 +13,20 @@ if (choice === 0) {
     return 'scissors';
 }
 }
-getComputerChoice();
+console.log(getComputerChoice()); 
 //create function named getComputerChoice 
 //write the code so that getComputerChoice will randomly return one of the following
-//string values: "rock" "paper" or "scissors"
+//string values: "rock" "paper" or "scissors" 
 //hint - the Math.random method returns a random number that's greater than or equal to 0
 //and less than 1. Think about how you can use this to conditionally return one of
 //the multiple choices. 
+
+//create new function named getHumanChoice 
+//write the code so that getHumanChoice will return one of the valid choices depending on
+//what the user inputs.
+//Hint: use the prompt method to get the user's input.
+//test what your function returns by using console.log
+
+function getHumanChoice() {
+    
+}
