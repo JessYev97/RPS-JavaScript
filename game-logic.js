51b60f6eac36ca627f4f87591de29bbc928f22a5
JessYev97 <jessmyeverino@gmail.com>
2024-06-19 14:33:17 -0500

    Write pseudocode

--- game-logic.js
+++ game-logic.js
@@ -1,3 +1,11 @@
 function greet() {
     console.log("Hello from external script!"); 
 }
+greet(); 
+//write a function that randomly returns 'rock' 'paper' or 'scissors' 
+//create function named getComputerChoice 
+//write the code so that getComputerChoice will randomly return one of the following
+//string values: "rock" "paper" or "scissors"
+//hint - the Math.random method returns a random number that's greater than or equal to 0
+//and less than 1. Think about how you can use this to conditionally return one of
+//the multiple choices. 
