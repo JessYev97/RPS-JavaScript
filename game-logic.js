fd711592d5544778eb5aac14b1efb5b562c25289
JessYev97 <jessmyeverino@gmail.com>
2024-06-25 16:03:29 -0500

    comment out unwanted code
    I am trying a new method tht I have been looking into and testing it along the way. I want to have my old code there just in case so for now I'm only commenting it out.

--- game-logic.js
+++ game-logic.js
@@ -5,39 +5,46 @@
  //const computerSelection = getComputerChoice();
  //after some research I found a better way to declare the choices above:
 const options = ["rock", "paper", "scissors"];
- playRound(humanSelection, computerSelection);
+ //playRound(humanSelection, computerSelection); (commenting this out temporarily)
 //write a function that randomly returns 'rock' 'paper' or 'scissors' 
 function getComputerChoice() {
-const randomNum = Math.floor(Math.random()  * 3);
-if (randomNum === 0) { 
-    return 'rock';
-} else if (randomNum === 1) {
-    return 'paper';
-} else {
-    return 'scissors';
+    //here I now need to tie in the new options const defined above:
+    const choice = options [Math.floor(Math.random()  * options.length)];
+    //above I changed the code; before it was * 3; this worked but this method's more intuitive
+    return choice; 
 }
-}
-console.log(getComputerChoice()); 
+getComputerChoice();
+//here I was on the right track but since I have changed the code to options above I'll 
+//comment the below code out.
+//if (randomNum === 0) { 
+  //  return 'rock';
+//} else if (randomNum === 1) {
+  //  return 'paper';
+//} else {
+  //  return 'scissors';
+//}
+//}
+//console.log(getComputerChoice()); 
 //getHumanChoice should return a value of "rock" "paper" or "scissors"
-function getHumanChoice() {
-   const humanInput = prompt("enter 'rock', 'paper' or 'scissors'"); 
-   return humanInput.toLowerCase();
+//function getHumanChoice() {
+  // const humanInput = prompt("enter 'rock', 'paper' or 'scissors'"); 
+   //return humanInput.toLowerCase();
   // it worked! wooo! happy times. Okay, commit then moving on. 
-}
+//}
 //initializing variables to 0 for humanScore and computerScore to keep track
 //of the players score (in the global scope)
-function playRound(humanInput, computerChoice) {
+//function playRound(humanInput, computerChoice) {
    // your code here! - thinking.
-   humanInput = humanSelection;
-   computerChoice = computerSelection; 
+  // humanInput = humanSelection;
+   //computerChoice = computerSelection; 
    //the above code is to give arguments to playRound from the global scope.
-   if (humanInput === 'rock' && computerChoice === 'scissors') {
-    return "You win!";
-   } else {
-    return "not a win, try again!";
-   }
- }
- console.log(playRound(humanInput, computerSelection)); 
+   //if (humanInput === 'rock' && computerChoice === 'scissors') {
+   // return "You win!";
+   //} else {
+    //return "not a win, try again!";
+   //}
+ //}
+ //console.log(playRound(humanInput, computerSelection)); 
  //starting with the win arguments; logging one string for all human win scenarios
 
  
